68f7eaa4f47bc208fba3bf8cad33845610db7006
Lewis Cook <35107692+LewisBCook@users.noreply.github.com>
2019-06-21 09:57:01 -0400

    Cleaned up the warnings in the build

--- VS2019.bat
+++ VS2019.bat
@@ -4,4 +4,10 @@ SET TargetVisualStudioNumericVersion=16.0
 SET TargetVisualStudioNumericNextVersion=17.0
 SET VSShortVersion=VS2019
 SET VSYear=2019
+
+SETX TargetVisualStudioNumericVersion %TargetVisualStudioNumericVersion% /M > NUL
+SETX TargetVisualStudioNumericNextVersion %TargetVisualStudioNumericNextVersion% /M > NUL
+SETX VSShortVersion %VSShortVersion% /M > NUL
+SETX VSYear %VSYear% /M > NUL
+
 IF NOT DEFINED TargetFrameworkVersion (SET TargetFrameworkVersion=v4.7.2)
